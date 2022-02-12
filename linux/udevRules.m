2c91d7b0eb1c3f14a81dd40835f7804c1f5b14de
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2022-02-12 10:33:49 -0600

    Update udevRules.m

--- linux/udevRules.m
+++ linux/udevRules.m
@@ -1,6 +1,6 @@
 /etc/udev/rules.d/01-ttyusb.rules 
 SUBSYSTEMS=="usb-serial", TAG+="uaccess"
 sudo usermod -a -G tty teamlary
-sudo usermod -a -G dialout your_user_name
+sudo usermod -a -G dialout teamlary
 udevadm control --reload
 reboot
